490bc1618fd73687553368ba5c259ba05c592ec2
mjo <mjo@goodmit.co.kr>
2025-02-02 00:28:57 +0900

    로그인 프로필선택추가

--- app/src/main/java/com/example/greetingcard/ui/theme/ui/login/component/LoginJoin.kt
+++ app/src/main/java/com/example/greetingcard/ui/theme/ui/login/component/LoginJoin.kt
@@ -1,23 +1,40 @@
 package com.example.greetingcard.ui.theme.ui.login.component
 
+import android.net.Uri
 import android.util.Log
 import android.util.Patterns
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,19 +44,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import coil3.compose.rememberAsyncImagePainter
 import com.example.greetingcard.data.model.dto.user.UserDTO
 import com.example.greetingcard.ui.theme.ui.login.view.CustomInputField
 import com.example.greetingcard.viewModel.login.LoginViewModel
-
 
 @Composable
 fun LoginJoin(
@@ -154,6 +175,11 @@ fun JoinForm(
                 Text("유요한 이메일 주소를 입력하세요.", color = MaterialTheme.colorScheme.error)
             }
         }
+
+
+        SetProfile()
+
+
         Button(
             modifier = modifier
                 .padding(top = 30.dp)
@@ -177,6 +203,8 @@ fun JoinForm(
                 fontWeight = FontWeight.Bold
             )
         }
+
+
         Button(
             modifier = modifier.padding(top = 30.dp),
             onClick = { navController.navigate("login") }) {
@@ -185,4 +213,108 @@ fun JoinForm(
     }
 }
 
+@Preview
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SetProfile() {
+    var showBottomSheet by remember { mutableStateOf(false) }
+    var selectedImageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val galleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri: Uri? ->
+        selectedImageUri = uri
+        showBottomSheet = false
+    }
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth(0.9f)
+            .padding(top = 15.dp)
+            .height(50.dp)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(2.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(50.dp)
+                    .clip(CircleShape)
+                    .border(2.dp, Color.Gray, CircleShape)
+            ) {
+                selectedImageUri?.let { uri ->
+                    Image(
+                        painter = rememberAsyncImagePainter(uri),
+                        contentDescription = "Selected Image",
+                        modifier = Modifier.fillMaxSize()
+                    )
+                } ?: run {
+                    Icon(
+                        imageVector = Icons.Filled.AddAPhoto,
+                        contentDescription = "Add Profile Image",
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .align(Alignment.Center),
+                        tint = Color.Gray
+                    )
+                }
+            }
+
+            OutlinedButton(
+                onClick = { showBottomSheet = !showBottomSheet },
+                modifier = Modifier
+                    .align(Alignment.CenterVertically)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(12.dp)
+            ) {
+                Text(
+                    text = "프로필 선택"
+                )
+            }
+        }
+
+
+        selectedImageUri?.let { uri ->
+            Image(
+                painter = rememberAsyncImagePainter(uri),
+                contentDescription = "Selected Image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .clip(RoundedCornerShape(16.dp))
+            )
+        }
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = {
+                showBottomSheet = false
+            }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp)
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text("이미지 선택", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+                Spacer(modifier = Modifier.height(10.dp))
+                Button(
+                    onClick = { galleryLauncher.launch("image/*") },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text("갤러리 열기")
+                }
+            }
+        }
+    }
+}
 
---
+++ app/src/main/java/com/example/greetingcard/ui/theme/ui/test/BasicLayout.kt
@@ -0,0 +1,55 @@
+package com.example.greetingcard.ui.theme.ui.test
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+fun ArtistCard() {
+    Text(
+        text = "레이아웃을 잡아주지 않으면",
+        modifier = Modifier.background(Color.White)
+    )
+    Text("겹치게 됩니다.")
+}
+
+
+@Preview
+@Composable
+fun ColumnCard() {
+    Column(modifier = Modifier.background(Color.White)) {
+        Text("Column은 수직정렬")
+        Text("수직정렬완료")
+    }
+}
+
+@Preview
+@Composable
+fun RowCard() {
+    Row(
+        modifier = Modifier.background(Color.White)
+    ) {
+            Text("Row는 수평정렬")
+            Text("수평정렬")
+        }
+}
+
+@Preview
+@Composable
+fun BoxCard() {
+    Box(
+        modifier = Modifier.background(Color.White)
+    ) {
+        Text("첫 번째 텍스트", modifier = Modifier.align(Alignment.TopStart))
+        Text("두 번째 텍스트", modifier = Modifier.align(Alignment.BottomEnd))
+    }
+
+}
